23009be3234b5777b4a3d8c361dfb024307075d5
Tryunfreodbsn77 <155061771+Tryunfreodbsn77@users.noreply.github.com>
2023-12-29 04:46:07 -0600

    Update Type.kt

--- ActivityEmbeddingWithPredictiveBack/app/src/main/java/com/google/sample/ae/pb/ui/theme/Type.kt
+++ ActivityEmbeddingWithPredictiveBack/app/src/main/java/com/google/sample/ae/pb/ui/theme/Type.kt
@@ -24,7 +24,7 @@ import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
+    bodyblur= TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
